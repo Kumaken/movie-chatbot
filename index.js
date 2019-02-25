b5634115cf3f18ddde057c066a334d145d060941
Stanley <30459659+Kumaken@users.noreply.github.com>
2019-02-25 19:22:11 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
-
+// Memakai express untuk membangun server!
 const express = require('express');
+// Memakai bodyParser untuk membaca dan memilah request-request yang akan diterima!
 const bodyParser = require('body-parser');
+// Memakai http untuk melakukan pemanggilan HTTP ke API untuk mengambil data
 const http = require('http');
+// Mendapatkan Api key dari OMDb API yang tersimpan di folder apiKey 
 const API_KEY = require('./apiKey');
 
+//Membuat server express:
 const server = express();
 server.use(bodyParser.urlencoded({
     extended: true
 }));
-
+//Parsing:
 server.use(bodyParser.json());
-
+// Route untuk POST request:
 server.post('/get-movie-details', (req, res) => {
-
-    const movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'The Godfather';
-    const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
+    // Akan diparse nama movie dari request body:
+    const targetTitle = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'The Godfather';
+    const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${targetTitle}&apikey=${API_KEY}`);
     http.get(reqUrl, (responseFromAPI) => {
-        let completeResponse = '';
+        let fullResponse = '';
         responseFromAPI.on('data', (chunk) => {
-            completeResponse += chunk;
+            fullResponse += chunk;
         });
         responseFromAPI.on('end', () => {
-            const movie = JSON.parse(completeResponse);
-            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
-            dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
+            const movie = JSON.parse(fullResponse);
+            let moviedetails = targetTitle === 'Dora The Explorer' ? `!!!Movie Not Found. Searching your secret favourite movie instead.!!!\n\n\n` : '';
+            moviedetails += `${movie.Title}\n\nRatings: ${movie.Rated}\nIMDB Rating: ${movie.imdbRating}\nBox Office: ${movie.BoxOffice}\nRelease Date: ${movie.Released}\n\nStarring: ${movie.Actors}\n\nType: ${movie.Type}\nGenres: ${movie.Genre}\nRuntime: ${movie.Runtime}\n\nDirector: ${movie.Director}\nWriter: ${movie.Writer}\n\nLanguages Available: ${movie.Language}\nCountry : ${movie.Country}\n\nSynopsis: ${movie.Plot}\n\nAwards won: ${movie.Awards}\nPoster: \n${movie.Poster}` ;
 
             return res.json({
-                fulfillmentText: dataToSend,
+                speech: moviedetails,
+                displayText: moviedetails,
                 source: 'get-movie-details'
             });
         });
     }, (error) => {
         return res.json({
-            fulfillmentText: 'Something went wrong!',
+            speech: 'Error woy! Hati-hati dong! :o',
+            displayText: 'Error woy! Hati-hati dong!  :o',
             source: 'get-movie-details'
         });
     });
 });
 
-
 server.listen((process.env.PORT || 8000), () => {
-    console.log("Server is up and running...");
+    console.log("Server berjalan dengan lancar");
 });
